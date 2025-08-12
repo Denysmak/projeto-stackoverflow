e4ebc2cb3c43ff20d7fa36c57593602664780d27
denys <denysmakene13@gmail.com>
2025-08-11 21:00:22 -0300

    change the query to get the year, language and how many times it showed that yeah

--- sql/data_extraction.sql
+++ sql/data_extraction.sql
@@ -1,4 +1,9 @@
-SELECT EXTRACT(YEAR FROM creation_date) AS year, tags 
-FROM bigquery-public-data.stackoverflow.posts_questions
+SELECT EXTRACT(YEAR FROM creation_date) AS year, tag
+,COUNT(*) as quantidade
+FROM bigquery-public-data.stackoverflow.posts_questions,
+
+UNNEST(SPLIT(tags, '|')) AS tag
+GROUP BY year, tag
 ORDER BY year
-LIMIT 20;
+
+LIMIT 200;
