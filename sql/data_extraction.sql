3b53d3a4368931d29bedc045f2f213cb81974cb8
denys <denysmakene13@gmail.com>
2025-08-16 17:52:58 -0300

    cria variavel com dataframe filtrado e outra com o dataframe com a quantidade de linguagens que aparecem por ano

--- sql/data_extraction.sql
+++ sql/data_extraction.sql
@@ -1,9 +1,7 @@
 SELECT EXTRACT(YEAR FROM creation_date) AS year, tag
 ,COUNT(*) as quantidade
 FROM bigquery-public-data.stackoverflow.posts_questions,
-
 UNNEST(SPLIT(tags, '|')) AS tag
 GROUP BY year, tag
 ORDER BY year
-
-LIMIT 200;
+;
